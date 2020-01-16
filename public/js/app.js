6a8d47496e746e1e627d36306569d828f09d5604
daniel.sarkadi@cubicfox.com <daniel.sarkadi@cubicfox.com>
2020-01-16 10:39:29 +0100

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit',(e) => {
     forecast.innerHTML = 'Loading';
     formlocation.innerHTML = '';
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    fetch(`/weather?address=${location}`).then(response => {
     response.json().then(data => {
         if(data.error) {
             forecast.innerHTML = data.error
